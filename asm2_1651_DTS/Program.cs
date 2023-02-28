b3061cadb0f7368b48b400e8d7498ee659469538
websiteblogdts <69079613+websiteblogdts@users.noreply.github.com>
2023-02-28 20:58:43 +0700

    push

---
+++ asm2_1651_DTS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace asm2_1651_DTS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ asm2_1651_DTS/UI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asm2_1651_DTS
+{
+    internal class UI
+    {
+    }
+}
